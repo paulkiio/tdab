984c29b82269e96a0e2a0db68d26ccc0c51a6282
Paul <pwathome@smarterhq.com>
2019-08-07 13:18:36 -0400

    refactor js for fetching resources

--- new_tab.js
+++ new_tab.js
@@ -1,3 +1,10 @@
 (function () {
+    let url = "http://quotes.rest/qod.json";
 
+    fetch(url)
+        .then(data => { return data.json() })
+        .then(res => {
+            let quoteElm = document.getElementById("quote");
+            quoteElm.textContent = res.contents.quotes[0].quote;
+        })
 })();
